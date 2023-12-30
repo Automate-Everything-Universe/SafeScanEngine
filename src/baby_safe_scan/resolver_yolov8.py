86274615611a70140dddd36aec1119070ed1a27b
Dragosjosan <dragosjosan@gmail.com>
2023-12-30 11:18:44 +0100

    Add json serializer

--- src/baby_safe_scan/resolver_yolov8.py
+++ src/baby_safe_scan/resolver_yolov8.py
@@ -2,12 +2,14 @@
 Resolver which uses YoloV8 Model.
 
 """
+import json
 from pathlib import Path
-from typing import List, Any, Union
+from typing import List, Dict, Any, Union
 
 from PIL.Image import Image
 from ultralytics import YOLO
 
+from .encoders import Base64Encoder
 from .folder_utils import convert_image, resize_image
 from .resolver import Resolver
 
@@ -29,6 +31,26 @@ class ImageProcessor:
         return pil_image
 
 
+class YoloResultSerializer:
+    def __init__(self, detections: List[Dict[str, Any]], labeled_image: bytes):
+        self.detections = detections
+        self.labeled_image = labeled_image
+
+    def to_json(self) -> str:
+        """
+        Converts the YOLO model results to a JSON-serializable format.
+
+        Returns:
+        - str: A JSON string representing the model results.
+        """
+        encoded_image = Base64Encoder.encode_image_to_base64(self.labeled_image)
+        data = {
+            "detections": self.detections,
+            "labeled_image": f"data:image/jpeg;base64,{encoded_image}"
+        }
+        return json.dumps(data)
+
+
 class YoloV8Resolver(Resolver):
     """
     YoloV8 Resolver
@@ -39,13 +61,16 @@ class YoloV8Resolver(Resolver):
         self.model = ModelInitializer.initialize_model()
 
     def process_images(self):
-        results = []
+        processed_data = []
         try:
             for image in self.images:
                 pil_image = ImageProcessor.convert_image(img=image)
-                result = self.run_model(img=pil_image)
-                results.append(result)
-            return results
+                detections, labeled_image = self.run_model(img=pil_image)
+                serializer = YoloResultSerializer(detections, labeled_image)
+                json_data = serializer.to_json()
+                processed_data.append(json_data)
+                processed_data.append(result_yolo)
+            return processed_data
         except Exception as e:
             print(f"Error processing image: {e}")
 
