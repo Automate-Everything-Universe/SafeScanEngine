d2e4606b06587d8cf03cad6d83d376bfa17f6f86
Dragosjosan <dragosjosan@gmail.com>
2023-12-31 15:13:49 +0100

    Make encoder work with pillow image object

--- src/baby_safe_scan/encoders.py
+++ src/baby_safe_scan/encoders.py
@@ -1,6 +1,7 @@
 import base64
 import io
 from pathlib import Path
+from typing import Union
 
 from PIL.Image import Image
 
@@ -9,8 +10,9 @@ class Base64Encoder:
     """
     Class to handle encoding from image to ASCI string.
     """
+
     @staticmethod
-    def encode_image_to_base64(image_path: Path) -> str:
+    def encode_image_to_base64(img: Image) -> str:
         """
         Encodes an image to a Base64 string.
 
@@ -21,9 +23,8 @@ class Base64Encoder:
         - str: The Base64 encoded string representation of the image.
         """
         # Open the image file and convert it to binary data
-        with Image.open(image_path) as image:
-            buffered = io.BytesIO()
-            image.save(buffered, format=image.format)
-            encoded_image = base64.b64encode(buffered.getvalue())
+        buffered = io.BytesIO()
+        img.save(buffered, format="png")
+        encoded_image = base64.b64encode(buffered.getvalue())
+        return encoded_image.decode("utf-8")
 
-            return encoded_image.decode('utf-8')
