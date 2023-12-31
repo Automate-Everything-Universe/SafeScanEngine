4691f11ee098ea37049d93ea403c92772e91a30c
Dragosjosan <dragosjosan@gmail.com>
2023-12-31 15:14:11 +0100

    Extend ImageProcessor to handle base64 conversions

--- src/baby_safe_scan/resolver_yolov8.py
+++ src/baby_safe_scan/resolver_yolov8.py
@@ -6,8 +6,11 @@ import json
 from pathlib import Path
 from typing import List, Dict, Any, Union
 
-from PIL.Image import Image
+import numpy as np
+import ultralytics
+from PIL import Image
 from ultralytics import YOLO
+from ultralytics.engine.results import Results, Boxes
 
 from .encoders import Base64Encoder
 from .folder_utils import convert_image, resize_image
@@ -24,15 +27,24 @@ class ModelInitializer:
 
 class ImageProcessor:
     @staticmethod
-    def convert_image(img: Union[Path, Image]) -> Image:
+    def convert_image_to_pillow(img: Image) -> Image:
         pil_image = convert_image(file=img)
         if pil_image.width > 640:
             pil_image = resize_image(img=pil_image, width=640)
         return pil_image
+    @staticmethod
+    def convert_image_to_base64(img: Union[Image.Image, np.ndarray]) -> str:
+        """
+        Encodes a Pillow Image or a Numpy array into an ASCII string
+        """
+        if isinstance(img, np.ndarray):
+            img = Image.fromarray(img, 'RGB')
+        encoded_image = Base64Encoder.encode_image_to_base64(img=img)
+        return encoded_image
 
 
 class YoloResultSerializer:
-    def __init__(self, detections: List[Dict[str, Any]], labeled_image: bytes):
+    def __init__(self, detections: List[Dict[str, Any]], labeled_image: str):
         self.detections = detections
         self.labeled_image = labeled_image
 
@@ -43,10 +55,9 @@ class YoloResultSerializer:
         Returns:
         - str: A JSON string representing the model results.
         """
-        encoded_image = Base64Encoder.encode_image_to_base64(self.labeled_image)
         data = {
             "detections": self.detections,
-            "labeled_image": f"data:image/jpeg;base64,{encoded_image}"
+            "labeled_image": f"data:image/jpeg;base64,{self.labeled_image}",
         }
         return json.dumps(data)
 
@@ -64,15 +75,15 @@ class YoloV8Resolver(Resolver):
         processed_data = []
         try:
             for image in self.images:
-                pil_image = ImageProcessor.convert_image(img=image)
-                detections, labeled_image = self.run_model(img=pil_image)
-                serializer = YoloResultSerializer(detections, labeled_image)
+                pil_image = ImageProcessor.convert_image_to_pillow(img=image)
+                detections = self.run_model(img=pil_image)
+                image_with_detection = detections[0].plot()
+                image_as_string = ImageProcessor.convert_image_to_base64(img=image_with_detection)
+                serializer = YoloResultSerializer(detections[0], image_as_string)
                 json_data = serializer.to_json()
-                processed_data.append(json_data)
-                processed_data.append(result_yolo)
             return processed_data
         except Exception as e:
             print(f"Error processing image: {e}")
 
     def run_model(self, img):
-        return self.model(source=img, show=True, conf=0.6, save=True, iou=0.4)
+        return self.model(source=img, show=False, conf=0.6, save=False, iou=0.4)
