a57e61e4dc24a39e6baa8d73c348d48489d9a0fa
Dragosjosan <dragosjosan@gmail.com>
2024-01-03 20:25:27 +0100

    Reformat the image processor

--- src/baby_safe_scan/resolver_yolov8.py
+++ src/baby_safe_scan/resolver_yolov8.py
@@ -8,42 +8,16 @@ from typing import List, Dict, Any, Union
 
 import numpy as np
 import torch
-from PIL import Image
-from PIL.Image import Image as PilImage
+
 from ultralytics import YOLO
 
 from ultralytics.engine.results import Results, Boxes
 
-from .encoders import Base64Encoder
-from .folder_utils import convert_image, resize_image
+from .image_processor import ImageProcessor
 from .model_initializer import YoloV8ModelInitializer, ELECTRICAL_OUTLET_MODEL
 from .model_initializer import ELECTRICAL_OUTLET_MODEL
 from .resolver import Resolver
 
-
-class ImageProcessor:
-    @staticmethod
-    def convert_images_to_pillow(images: List[PilImage]) -> List[PilImage]:
-        processed_images = []
-        for image in images:
-            pil_image = convert_image(file=image)
-            if pil_image.width > 640:
-                pil_image = resize_image(img=pil_image, width=640)
-                processed_images.append(pil_image)
-            else:
-                processed_images.append(pil_image)
-
-        return processed_images
-
-    @staticmethod
-    def convert_image_to_base64(img: Union[Image.Image, np.ndarray]) -> str:
-        """
-        Encodes a Pillow Image or a Numpy array into an ASCII string
-        """
-        if isinstance(img, np.ndarray):
-            img = Image.fromarray(img, "RGB")
-        encoded_image = Base64Encoder.encode_image_to_base64(img=img)
-        return encoded_image
 
 
 class YoloResultSerializer:
@@ -83,7 +57,7 @@ class YoloV8Resolver(Resolver):
             processed_detections = []
             for detection in self.detections:
                 if YoloV8Resolver.detections_is_available(detection=detection):
-                    labeled_image = self.extract_labeled_image(detections=detection)
+                    labeled_image = ImageProcessor.extract_labeled_image(detections=detection)
                     detections_to_process = self.extract_detections_for_serializer(
                         detections=detection
                     )
@@ -106,13 +80,6 @@ class YoloV8Resolver(Resolver):
         if not detection.boxes.shape[0]:
             return False
         return True
-
-    @staticmethod
-    def extract_labeled_image(detections):
-        try:
-            return detections[0].plot()
-        except Exception as exc:
-            print(f"Could not extract labeled image: {exc}")
 
     @staticmethod
     def extract_detections_for_serializer(detections: Results) -> object:
