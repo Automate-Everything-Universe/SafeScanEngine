04c5aa8bca2aa1895a70cbe3e8cc184b6cd9bd2f
dragosjosan <Dragosjosan@gmail.com>
2024-01-09 21:37:06 +0100

    Add assert statement

--- tests/test_yolov8resolver.py
+++ tests/test_yolov8resolver.py
@@ -24,11 +24,4 @@ def input_path() -> Path:
 def test_yolovresolver(img_1, img_2):
     electrical_outlet_resolver = YoloV8Resolver(images=[img_1, img_2])
     results = electrical_outlet_resolver.create_json_object()
-    print("")
-
-    # Check if processed image was created
-    # assert expected_pillow.exists(), "Processed image file does not exist"
-
-    # Clean up
-    # os.remove(expected_pillow)
-    # shutil.rmtree()
+    assert len(results) >= 1, "No result was extracted"
