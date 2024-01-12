d60f22a4f6222aa6e32917a9c7710ef0fec58fae
dragosjosan <Dragosjosan@gmail.com>
2024-01-12 21:23:41 +0100

    Add room electrical outlet

--- tests/test_yolov8resolver.py
+++ tests/test_yolov8resolver.py
@@ -17,11 +17,15 @@ def img_2() -> Path:
 
 
 @pytest.fixture
+def img_no_danger() -> Path:
+    return TEST_FOLDER / "pic/empty_living_room.jpeg"
+
+@pytest.fixture
 def input_path() -> Path:
     return TEST_FOLDER / "pic/"
 
 
-def test_yolovresolver(img_1, img_2):
-    electrical_outlet_resolver = YoloV8Resolver(images=[img_1, img_2])
+def test_yolovresolver(img_1, img_2, img_no_danger):
+    electrical_outlet_resolver = YoloV8Resolver(images=[img_1, img_no_danger, img_2])
     results = electrical_outlet_resolver.return_detections()
     assert len(results) >= 1, "No result was extracted"
